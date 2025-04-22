9cc04a62159a34afa1cdbbaddec729456372f9f0
ani8727 <aniketgupta.8727@gmail.com>
2025-04-22 12:23:21 +0530

    Project Done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+
+        int myNumber  = (int)(Math.random()*100);
+        int userNumber = 0;
+
+        do {
+            System.out.println("Guess my number(1-100)");
+            userNumber = sc.nextInt();
+
+            if(userNumber == myNumber) {
+                System.out.println("Good .. Correct Number!!");
+                break;
+            }
+            else if(userNumber>myNumber) {
+                System.out.println("Your Number ism to large");
+            }
+            else {
+                System.out.println("Your number is too small");
+            }
+
+        } while(userNumber >= 0);
+
+        System.out.println("My number was: ");
+        System.out.println(myNumber);
     }
 }
